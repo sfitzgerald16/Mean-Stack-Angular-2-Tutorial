f7dbf93dd739b67ee6f0f6ddabce4d125417b2ff
sfitzgerald16 <misfitz616@gmail.com>
2017-08-24 20:45:22 -0700

    added form and validation

--- client/src/app/app.routing.module.ts
+++ client/src/app/app.routing.module.ts
@@ -2,9 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '**', component: HomeComponent },
 ];
