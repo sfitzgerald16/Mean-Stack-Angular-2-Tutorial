b45258ee5261adc8b561e390ad9364a8c1f4e368
sfitzgerald16 <misfitz616@gmail.com>
2017-09-11 19:51:35 -0700

    edited home page

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent implements OnInit {
           if (this.previousUrl) {
             this.router.navigate([this.previousUrl]);
           } else {
-            this.router.navigate(['/dashboard']);
+            this.router.navigate(['/blogs']);
           }
         }, 2000);
       }
